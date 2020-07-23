6a3c2004c33686ce33595eda561b81be5a059c4b
Arjun Khandkar <khandkararjun@gmail.com>
2020-07-24 02:20:34 +0530

    fix scrolling, use Brick.Widgets.List instead of EntryCursor

--- src/BrowserInterface.hs
+++ src/BrowserInterface.hs
@@ -2,8 +2,6 @@
 
 module BrowserInterface where
 
-import EntryCursor
-
 import System.Directory
 import System.Exit (die)
 
@@ -11,15 +9,17 @@ import Data.List (sort)
 import qualified Data.List as DL
 import Data.List.NonEmpty (NonEmpty(..))
 import qualified Data.List.NonEmpty as NE
+import qualified Data.Vector as V (fromList)
 
 import Control.Monad.IO.Class (MonadIO, liftIO)
 
-import Brick.AttrMap (attrMap)
+import Brick.AttrMap (AttrMap, attrMap)
 import Brick.Main
-import Brick.Types (BrickEvent(..), EventM, Next, ViewportType(..), Widget)
+import Brick.Types (BrickEvent(..), EventM, Next, ViewportType(..), Widget, vpSize)
 import Brick.Util (on)
 import Brick.Widgets.Center (hCenter)
 import Brick.Widgets.Core (emptyWidget, raw, str, vBox, viewport, withAttr)
+import qualified Brick.Widgets.List as L
 
 import qualified Graphics.Vty as GV
 import Graphics.Vty.Attributes
@@ -34,12 +34,13 @@ data BrowserState =
     BrowserState
         { status :: Status
         , currDir :: FilePath
-        , entryCursor :: EntryCursor FilePath
+        , dirList :: L.List ResourceName FilePath
         }
-    deriving (Show, Eq)
+    deriving (Show)
 
-data ResourceName =
-    ListViewport
+data ResourceName
+    = ListViewport
+    | DirList
     deriving (Show, Eq, Ord)
 
 runBrowser :: IO ()
@@ -55,29 +56,30 @@ browserInterface =
         , appChooseCursor = showFirstCursor
         , appHandleEvent = handleEv
         , appStartEvent = pure
-        , appAttrMap =
-              const $
-              attrMap
-                  mempty
-                  [ ("selected", GV.black `on` GV.white)
-                  , ("title", GV.withStyle (GV.white `on` GV.blue) GV.bold)
-                  , ("statusOK", GV.withStyle (GV.white `on` GV.green) GV.bold)
-                  , ("statusNotOK", GV.withStyle (GV.white `on` GV.red) GV.bold)
-                  ]
+        , appAttrMap = const attributes
         }
+
+attributes :: AttrMap
+attributes =
+    attrMap
+        GV.defAttr
+        [ (L.listSelectedAttr, GV.black `on` GV.white)
+        , ("title", GV.withStyle (GV.white `on` GV.blue) GV.bold)
+        , ("statusOK", GV.withStyle (GV.white `on` GV.green) GV.bold)
+        , ("statusNotOK", GV.withStyle (GV.white `on` GV.red) GV.bold)
+        ]
 
 initialize :: IO BrowserState
 initialize = do
     cd <- getCurrentDirectory
     paths <- DL.sort <$> (getDirectoryContents cd)
-    case NE.nonEmpty paths of
-        Nothing -> die "Empty directory list"
-        Just p' -> return BrowserState {status = OK, currDir = cd, entryCursor = makeCursor $ NE.toList p'}
+    if null paths
+        then die "Empty directory list"
+        else return $ BrowserState OK cd (L.list DirList (V.fromList paths) 1)
 
 render :: BrowserState -> [Widget ResourceName]
 render s =
     let title = "sbrowse 0.1.0.0"
-        eCurs = entryCursor s
      in flip (:) [] $
         vBox
             [ vBox
@@ -86,65 +88,40 @@ render s =
                         then (withAttr "statusOK") . hCenter . str $ "Browsing" ++ currDir s
                         else (withAttr "statusNotOK") . hCenter . str $ (\(NotOK s) -> s) $ status s
                   ]
-            , viewport ListViewport Vertical $
-              vBox $
-              concat
-                  [ str <$> (reverse $ prevEntries eCurs)
-                  , flip (:) [] $ (withAttr "selected") . str $ currEntry eCurs
-                  , str <$> (nextEntries eCurs)
-                  ]
+            , (L.renderList listDrawElement True (dirList s))
             ]
 
+listDrawElement :: Bool -> FilePath -> Widget ResourceName
+listDrawElement s e = str $ e
+
 handleEv :: BrowserState -> BrickEvent ResourceName e -> EventM ResourceName (Next BrowserState)
-handleEv s e =
+handleEv s e = do
     case e of
-        VtyEvent vtye ->
-            case vtye of
+        VtyEvent ev ->
+            case ev of
                 EvKey (KChar 'q') [] -> halt s
-                EvKey KDown [] -> do
-                    vScrollBy (viewportScroll ListViewport) 1
-                    continue $ select nextEntry s
-                EvKey KUp [] -> do
-                    vScrollBy (viewportScroll ListViewport) (-1)
-                    continue $ select prevEntry s
                 EvKey KEnter [] -> do
                     s' <- liftIO $ enterDir s
-                    if status s' == OK
-                        then do
-                            vScrollToBeginning (viewportScroll ListViewport)
-                        else pure ()
                     continue s'
-                _ -> continue s
+                ev -> do
+                    e <- L.handleListEvent ev (dirList s)
+                    continue s {dirList = e}
         _ -> continue s
-
-select :: (EntryCursor FilePath -> Maybe (EntryCursor FilePath)) -> BrowserState -> BrowserState
-select d s =
-    let eCurs = entryCursor s
-     in case d eCurs of
-            Nothing -> s
-            Just e' -> s {status = OK, currDir = currDir s, entryCursor = e'}
 
 enterDir :: BrowserState -> IO BrowserState
 enterDir s = do
-    let eCurs = entryCursor s
-    isDirectory <- doesDirectoryExist $ currEntry eCurs
-    isReadable <- ((getPermissions $ currEntry eCurs) >>= (\p -> pure $ readable p))
-    if isReadable
-        then do
-            if isDirectory
+    let mbSelection = L.listSelectedElement $ dirList s
+    case mbSelection of
+        Nothing -> return s
+        Just (_, currEntry) -> do
+            isDirectory <- doesDirectoryExist currEntry
+            isReadable <- ((getPermissions $ currEntry) >>= (\p -> pure $ readable p))
+            if isReadable
                 then do
-                    setCurrentDirectory $ currEntry eCurs
-                    s' <- initialize
-                    pure s'
-                else pure $
-                     s
-                         { status = NotOK $ "Cannot open: '" ++ (currEntry eCurs) ++ "' is a file"
-                         , currDir = currDir s
-                         , entryCursor = eCurs
-                         }
-        else pure $
-             s
-                 { status = NotOK $ "Cannot open '" ++ (currEntry eCurs) ++ "': permission denied"
-                 , currDir = currDir s
-                 , entryCursor = eCurs
-                 }
+                    if isDirectory
+                        then do
+                            setCurrentDirectory $ currEntry
+                            s' <- initialize
+                            pure s'
+                        else pure $ s {status = NotOK $ "Cannot open: '" ++ currEntry ++ "' is a file"}
+                else pure $ s {status = NotOK $ "Cannot open '" ++ currEntry ++ "': permission denied"}
