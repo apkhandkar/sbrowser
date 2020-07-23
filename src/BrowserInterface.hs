906c1b29beb1e48f55bc5951b8383fca9162a503
Arjun Khandkar <khandkararjun@gmail.com>
2020-07-23 19:02:23 +0530

    hindent'ed code

--- src/BrowserInterface.hs
+++ src/BrowserInterface.hs
@@ -9,53 +9,62 @@ import System.Exit (die)
 
 import Data.List (sort)
 import qualified Data.List as DL
-import Data.List.NonEmpty (NonEmpty (..))
+import Data.List.NonEmpty (NonEmpty(..))
 import qualified Data.List.NonEmpty as NE
 
 import Control.Monad.IO.Class (MonadIO, liftIO)
 
-import Brick.Main
-import Brick.Util (on)
-import Brick.Types (Widget, BrickEvent (..), EventM, Next, ViewportType (..))
 import Brick.AttrMap (attrMap)
-import Brick.Widgets.Core (emptyWidget, raw, str, withAttr, vBox, viewport)
+import Brick.Main
+import Brick.Types (BrickEvent(..), EventM, Next, ViewportType(..), Widget)
+import Brick.Util (on)
 import Brick.Widgets.Center (hCenter)
+import Brick.Widgets.Core (emptyWidget, raw, str, vBox, viewport, withAttr)
 
+import qualified Graphics.Vty as GV
 import Graphics.Vty.Attributes
 import Graphics.Vty.Input.Events
-import qualified Graphics.Vty as GV
 
-data Status = OK | NotOK String
+data Status
+    = OK
+    | NotOK String
     deriving (Show, Eq)
 
-data BrowserState = BrowserState
-    {   status      :: Status
-    ,   currDir     :: FilePath
-    ,   entryCursor :: EntryCursor FilePath
-    } deriving (Show, Eq)
+data BrowserState =
+    BrowserState
+        { status :: Status
+        , currDir :: FilePath
+        , entryCursor :: EntryCursor FilePath
+        }
+    deriving (Show, Eq)
 
-data ResourceName = ListViewport
+data ResourceName =
+    ListViewport
     deriving (Show, Eq, Ord)
 
 runBrowser :: IO ()
 runBrowser = do
     initState <- initialize
     finalState <- defaultMain browserInterface initState
-    pure()
+    pure ()
 
 browserInterface :: App BrowserState e ResourceName
-browserInterface = App
-    {   appDraw         = render
-    ,   appChooseCursor = showFirstCursor
-    ,   appHandleEvent  = handleEv
-    ,   appStartEvent   = pure
-    ,   appAttrMap      = const $ attrMap mempty
-            [   ("selected", GV.black `on` GV.white)
-            ,   ("title", GV.withStyle (GV.white `on` GV.blue) GV.bold)
-            ,   ("statusOK", GV.withStyle (GV.white `on` GV.green) GV.bold)
-            ,   ("statusNotOK", GV.withStyle (GV.white `on` GV.red) GV.bold)
-            ]
-    }
+browserInterface =
+    App
+        { appDraw = render
+        , appChooseCursor = showFirstCursor
+        , appHandleEvent = handleEv
+        , appStartEvent = pure
+        , appAttrMap =
+              const $
+              attrMap
+                  mempty
+                  [ ("selected", GV.black `on` GV.white)
+                  , ("title", GV.withStyle (GV.white `on` GV.blue) GV.bold)
+                  , ("statusOK", GV.withStyle (GV.white `on` GV.green) GV.bold)
+                  , ("statusNotOK", GV.withStyle (GV.white `on` GV.red) GV.bold)
+                  ]
+        }
 
 initialize :: IO BrowserState
 initialize = do
@@ -63,39 +72,35 @@ initialize = do
     paths <- DL.sort <$> (getDirectoryContents cd)
     case NE.nonEmpty paths of
         Nothing -> die "Empty directory list"
-        Just p' -> return BrowserState
-            {   status      = OK
-            ,   currDir     = cd
-            ,   entryCursor = makeCursor $ NE.toList p'
-            }
+        Just p' -> return BrowserState {status = OK, currDir = cd, entryCursor = makeCursor $ NE.toList p'}
 
 render :: BrowserState -> [Widget ResourceName]
-render s = 
-    let
-        title = "sbrowse 0.1.0.0"
+render s =
+    let title = "sbrowse 0.1.0.0"
         eCurs = entryCursor s
-    in flip (:) [] $ vBox
-        [   vBox
-            [   (withAttr "title") . hCenter . str $ title
-            ,   if status s == OK then
-                    (withAttr "statusOK") . hCenter . str $ "Browsing" ++ currDir s
-                else
-                    (withAttr "statusNotOK") . hCenter . str $ (\(NotOK s) -> s) $ status s
+     in flip (:) [] $
+        vBox
+            [ vBox
+                  [ (withAttr "title") . hCenter . str $ title
+                  , if status s == OK
+                        then (withAttr "statusOK") . hCenter . str $ "Browsing" ++ currDir s
+                        else (withAttr "statusNotOK") . hCenter . str $ (\(NotOK s) -> s) $ status s
+                  ]
+            , viewport ListViewport Vertical $
+              vBox $
+              concat
+                  [ str <$> (reverse $ prevEntries eCurs)
+                  , flip (:) [] $ (withAttr "selected") . str $ currEntry eCurs
+                  , str <$> (nextEntries eCurs)
+                  ]
             ]
-        ,   viewport ListViewport Vertical $ vBox $ concat
-            [   str <$> (reverse $ prevEntries eCurs)
-            ,   flip (:) [] $ (withAttr "selected") . str $ currEntry eCurs
-            ,   str <$> (nextEntries eCurs)
-            ]
-        ]
 
 handleEv :: BrowserState -> BrickEvent ResourceName e -> EventM ResourceName (Next BrowserState)
 handleEv s e =
     case e of
         VtyEvent vtye ->
             case vtye of
-                EvKey (KChar 'q') [] ->
-                    halt s
+                EvKey (KChar 'q') [] -> halt s
                 EvKey KDown [] -> do
                     vScrollBy (viewportScroll ListViewport) 1
                     continue $ select nextEntry s
@@ -104,48 +109,42 @@ handleEv s e =
                     continue $ select prevEntry s
                 EvKey KEnter [] -> do
                     s' <- liftIO $ enterDir s
-                    if status s' == OK then do
-                        vScrollToBeginning (viewportScroll ListViewport)
-                    else
-                        pure ()
+                    if status s' == OK
+                        then do
+                            vScrollToBeginning (viewportScroll ListViewport)
+                        else pure ()
                     continue s'
-                _ ->
-                    continue s
-        _ ->
-            continue s
+                _ -> continue s
+        _ -> continue s
 
-select :: (EntryCursor FilePath -> Maybe (EntryCursor FilePath))
-    -> BrowserState
-    -> BrowserState
+select :: (EntryCursor FilePath -> Maybe (EntryCursor FilePath)) -> BrowserState -> BrowserState
 select d s =
-    let eCurs = entryCursor s in
-    case d eCurs of
-        Nothing -> s
-        Just e' -> s
-            {   status      = OK
-            ,   currDir     = currDir s
-            ,   entryCursor = e'
-            }
+    let eCurs = entryCursor s
+     in case d eCurs of
+            Nothing -> s
+            Just e' -> s {status = OK, currDir = currDir s, entryCursor = e'}
 
 enterDir :: BrowserState -> IO BrowserState
 enterDir s = do
     let eCurs = entryCursor s
     isDirectory <- doesDirectoryExist $ currEntry eCurs
     isReadable <- ((getPermissions $ currEntry eCurs) >>= (\p -> pure $ readable p))
-    if isReadable then do
-        if isDirectory then do
-            setCurrentDirectory $ currEntry eCurs
-            s' <- initialize
-            pure s'
-        else
-            pure $ s
-            {   status      = NotOK $ "Cannot open: '" ++ (currEntry eCurs) ++ "' is a file"
-            ,   currDir     = currDir s
-            ,   entryCursor = eCurs
-            }
-    else
-        pure $ s
-        {   status      = NotOK $ "Cannot open '" ++ (currEntry eCurs) ++ "': permission denied"
-        ,   currDir     = currDir s
-        ,   entryCursor = eCurs
-        }
+    if isReadable
+        then do
+            if isDirectory
+                then do
+                    setCurrentDirectory $ currEntry eCurs
+                    s' <- initialize
+                    pure s'
+                else pure $
+                     s
+                         { status = NotOK $ "Cannot open: '" ++ (currEntry eCurs) ++ "' is a file"
+                         , currDir = currDir s
+                         , entryCursor = eCurs
+                         }
+        else pure $
+             s
+                 { status = NotOK $ "Cannot open '" ++ (currEntry eCurs) ++ "': permission denied"
+                 , currDir = currDir s
+                 , entryCursor = eCurs
+                 }
--- src/EntryCursor.hs
+++ src/EntryCursor.hs
@@ -1,36 +1,34 @@
 module EntryCursor where
 
-data EntryCursor a = EntryCursor
-    {   prevEntries :: [a]
-    ,   currEntry   :: a
-    ,   nextEntries :: [a]
-    } deriving (Show, Eq)
+data EntryCursor a =
+    EntryCursor
+        { prevEntries :: [a]
+        , currEntry :: a
+        , nextEntries :: [a]
+        }
+    deriving (Show, Eq)
 
 makeCursor :: (Show a, Eq a) => [a] -> EntryCursor a
-makeCursor li = EntryCursor
-    {   prevEntries = []
-    ,   currEntry   = head li
-    ,   nextEntries = tail li
-    }
+makeCursor li = EntryCursor {prevEntries = [], currEntry = head li, nextEntries = tail li}
 
 nextEntry :: (Eq a) => EntryCursor a -> Maybe (EntryCursor a)
 nextEntry e
-    | nextEntries e == [] =
-        Nothing
+    | nextEntries e == [] = Nothing
     | otherwise =
-        Just EntryCursor
-        {   prevEntries = (currEntry e):(prevEntries e)
-        ,   currEntry   = head $ nextEntries e
-        ,   nextEntries = tail $ nextEntries e
-        }
+        Just
+            EntryCursor
+                { prevEntries = (currEntry e) : (prevEntries e)
+                , currEntry = head $ nextEntries e
+                , nextEntries = tail $ nextEntries e
+                }
 
 prevEntry :: (Eq a) => EntryCursor a -> Maybe (EntryCursor a)
 prevEntry e
-    | prevEntries e == [] =
-        Nothing
-    | otherwise = 
-        Just EntryCursor
-        {   prevEntries = tail $ prevEntries e
-        ,   currEntry   = head $ prevEntries e
-        ,   nextEntries = (currEntry e):(nextEntries e)
-        }
+    | prevEntries e == [] = Nothing
+    | otherwise =
+        Just
+            EntryCursor
+                { prevEntries = tail $ prevEntries e
+                , currEntry = head $ prevEntries e
+                , nextEntries = (currEntry e) : (nextEntries e)
+                }
