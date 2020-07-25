44aa62c886ec96abdaa5ab0989ca1d6c4d74533f
Arjun Khandkar <khandkararjun@gmail.com>
2020-07-26 02:21:31 +0530

    add timestamp formatting function

--- src/SBrowser/Utils.hs
+++ src/SBrowser/Utils.hs
@@ -1,6 +1,8 @@
 module SBrowser.Utils where
 
 import qualified Data.List as DL
+import Data.Time.Clock
+import Data.Time.Format
 import SBrowser.FileInfo (FileInfo(..))
 
 fileInfoString :: Maybe FileInfo -> String
@@ -10,3 +12,6 @@ fileInfoString (Just fi) =
     if (desc fi) == "directory"
         then ""
         else ", " ++ (show $ size fi) ++ " bytes"
+
+timeToString :: UTCTime -> String
+timeToString = formatTime (defaultTimeLocale) "%r %Z, %d-%m-%Y"
