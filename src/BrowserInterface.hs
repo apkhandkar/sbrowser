17c4e4fc902ed22d8df8b1556de03514bddc1485
Arjun Khandkar <khandkararjun@gmail.com>
2020-07-25 10:25:15 +0530

    refactoring

--- src/BrowserInterface.hs
+++ src/BrowserInterface.hs
@@ -2,17 +2,6 @@
 
 module BrowserInterface where
 
-import System.Directory
-import System.Exit (die)
-
-import Data.List (sort)
-import qualified Data.List as DL
-import Data.List.NonEmpty (NonEmpty(..))
-import qualified Data.List.NonEmpty as NE
-import qualified Data.Vector as V (fromList)
-
-import Control.Monad.IO.Class (MonadIO, liftIO)
-
 import Brick.AttrMap (AttrMap, attrMap)
 import Brick.Main
 import Brick.Types (BrickEvent(..), EventM, Next, ViewportType(..), Widget, vpSize)
@@ -20,11 +9,18 @@ import Brick.Util (on)
 import Brick.Widgets.Border
 import Brick.Widgets.Center (hCenter)
 import Brick.Widgets.Core (emptyWidget, padLeftRight, raw, str, vBox, viewport, withAttr)
-import qualified Brick.Widgets.List as L
-
+import qualified Brick.Widgets.List as WL
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Data.List (sort)
+import qualified Data.List as DL
+import Data.List.NonEmpty (NonEmpty(..))
+import qualified Data.List.NonEmpty as NE
+import qualified Data.Vector as DV (fromList)
 import qualified Graphics.Vty as GV
 import Graphics.Vty.Attributes
 import Graphics.Vty.Input.Events
+import System.Directory
+import System.Exit (die)
 
 versionString :: String
 versionString = "0.1.0.0"
@@ -38,7 +34,7 @@ data BrowserState =
     BrowserState
         { status :: Status
         , currDir :: FilePath
-        , dirList :: L.List ResourceName FilePath
+        , dirList :: WL.List ResourceName FilePath
         }
     deriving (Show)
 
@@ -67,7 +63,7 @@ attributes :: AttrMap
 attributes =
     attrMap
         GV.defAttr
-        [ (L.listSelectedAttr, GV.black `on` GV.white)
+        [ (WL.listSelectedAttr, GV.black `on` GV.white)
         , ("title", GV.withStyle (GV.white `on` GV.blue) GV.bold)
         , ("statusOK", GV.withStyle (GV.white `on` GV.green) GV.bold)
         , ("statusNotOK", GV.withStyle (GV.white `on` GV.red) GV.bold)
@@ -79,7 +75,7 @@ initialize = do
     paths <- DL.sort <$> (getDirectoryContents cd)
     if null paths
         then die "Empty directory list"
-        else return $ BrowserState OK cd (L.list DirList (V.fromList paths) 1)
+        else return $ BrowserState OK cd (WL.list DirList (DV.fromList paths) 1)
 
 render :: BrowserState -> [Widget ResourceName]
 render s =
@@ -91,13 +87,10 @@ render s =
                     then (withAttr "statusOK") . hCenter . str $ "Browsing " ++ currDir s
                     else (withAttr "statusNotOK") . hCenter . str $ (\(NotOK s) -> s) $ status s
               ]
-        , L.renderList listDrawElement True (dirList s)
+        , WL.renderList (\s e -> str $ e) True (dirList s)
         ]
   where
     title = "sbrowse v" ++ versionString
-
-listDrawElement :: Bool -> FilePath -> Widget ResourceName
-listDrawElement s e = str $ e
 
 handleEv :: BrowserState -> BrickEvent ResourceName e -> EventM ResourceName (Next BrowserState)
 handleEv s e = do
@@ -105,17 +98,16 @@ handleEv s e = do
         VtyEvent ev ->
             case ev of
                 EvKey (KChar 'q') [] -> halt s
-                EvKey KEnter [] -> do
-                    s' <- liftIO $ enterDir s
-                    continue s'
-                ev -> do
-                    e <- L.handleListEvent ev (dirList s)
-                    continue s {dirList = e}
+                EvKey KEnter [] -> continue =<< (liftIO $ enterDir s)
+                ev -> continue =<< handleNavigation s ev
         _ -> continue s
+
+handleNavigation :: BrowserState -> Event -> EventM ResourceName BrowserState
+handleNavigation s e = WL.handleListEventVi WL.handleListEvent e (dirList s) >>= \l -> return $ s {dirList = l}
 
 enterDir :: BrowserState -> IO BrowserState
 enterDir s = do
-    let mbSelection = L.listSelectedElement $ dirList s
+    let mbSelection = WL.listSelectedElement $ dirList s
     case mbSelection of
         Nothing -> return s
         Just (_, currEntry) -> do
